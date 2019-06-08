8db3451c80d015005f92f37511f9c6fcad9f169a
Jorge Miguel <jorgemprolhas@gmail.com>
2019-06-08 16:17:23 +0100

    Prepare project to have multiple stores, Remove Logos

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-toolbar dark color="primary" app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="headline">
+        <span>CicloClube BTT Elvas</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
