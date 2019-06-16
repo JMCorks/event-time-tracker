511902cecb9bb7b10d92fe95607b15779bd7b155
Jorge Miguel <jorgemprolhas@gmail.com>
2019-06-16 17:24:05 +0100

    Fixes

--- src/models/Event/EventModel.ts
+++ src/models/Event/EventModel.ts
@@ -1,8 +1,8 @@
 import { DbModel } from '../db/DbModel';
 
 export class EventModel extends DbModel {
-    public title: string = "";
-    public date: string = "";
-    public details: string = "";
-    public organizerEmail: string = "";
+    public title: string;
+    public date: string;
+    public details: string;
+    public organizerEmail: string;
 }
